cc34681aa1330592b164bba30f52f1db5b74ea4c
dtsbourg <dylan.bourgeois@epfl.ch>
2015-11-24 14:44:44 +0100

    update usage() to match readme info

--- seglog.py
+++ seglog.py
@@ -22,6 +22,7 @@ def usage():
 
         `output_path` is the current directory
         headers are displayed on each split file
+        segmented files are prefixed with `flight-{n}.txt`
 
         Available operators: > / >= / < / <= / == / !=
 
@@ -29,7 +30,7 @@ def usage():
 
         ```
         >> python seglog.py -i log.txt -o flight -p "state >= 0 && acc_x > 20"
-        >> python seglog.py -i Log_file_201.txt -p "gps_status==2 || healthy != 0"
+        >> python seglog.py -i ../Desktop/Log_file_201.txt -p "gps_status==2 || healthy != 0"
         ```
 
     """
